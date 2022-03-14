700e96a235a10c7bbfdc9f2f25bd24bf8acba846
Thiago Cesar <andrade-tc@uol.com.br>
2022-03-14 08:22:43 -0300

    commit

--- cypress/integration/produtos/produtos.step.js
+++ cypress/integration/produtos/produtos.step.js
@@ -1,34 +1,48 @@
 /// <reference types="cypress" />
 
-import { addProduct } from '../../support/commands'
 import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
 const { carrinhoPage, produtosPage } = require('../../support/page_objects')
-const prodIntercept = require('../../fixtures/prodIntercept.json')
-const produtos = require('../../fixtures/produtos.json')
+const dados = require('../../fixtures/prodIntercept.json')
 
 Given('I acces the product page', () => {
-    cy.visit('/produtos')
+    cy.visit('/product/abominable-hoodie/')
 
 })
 
 When('I add a product in the cart', () => {
     cy.intercept({
         method: 'POST',
-        url: '?wc-ajax=get_refreshed_fragments',         
+        url: '/wp-admin/admin-ajax*',         
     }, req => {
         if(req.headers.cookie.includes("woocommerce_items_in_cart=1")){
             req.reply({
                 statusCode: 200,
-                body: JSON.stringify(prodIntercept.proddata)
+                body: dados.response
             })
         }        
-    }).as('addProd') 
+    })
     
-    
-    produtosPage.inserirProduto(produtos[1].produto, produtos[1].tamanho,
-        produtos[1].cor, produtos[1].quantidade)
+    cy.intercept({
+        method: 'POST',
+        url: '/?wc-ajax=get_refreshed_fragments*',         
+    }, req => {
+                req.reply({
+                statusCode: 200,
+                body: dados.html
+            })
+    })
 
-    cy.wait('@addProd')
+    cy.intercept({
+        method: 'POST',
+        url: '/product/abominable-hoodie/',         
+    },  req => {
+        window.sessionStorage.setItem("wc_fragments_a84fb9b97c9e7516ea041e13a46d5c80", dados.html)     
+        req.reply(     
+         {     
+          statusCode: 200,     
+          body: htmlRespostaSubmit     
+         })     
+       })    
 })
 
 Then('In the cart I must see the product', () => {
